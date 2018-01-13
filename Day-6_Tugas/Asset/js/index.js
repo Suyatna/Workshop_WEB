02128b83e11e7cbb34ce454aa3de11c198efb197
Suyatna <zuyatna@gmail.com>
2018-01-13 22:28:16 +0700

    first add

---
+++ Day-6_Tugas/Asset/js/index.js
@@ -0,0 +1,54 @@
+// window.onload = function() {
+//    document.getElementById('demo').innerHTML = "hello";
+// }
+
+// // variabel
+// var x = "hello";
+// var y = 12;
+// var nama = "Suyatna";
+
+//document.write(`hello ${nama}`);
+
+// //objek
+// var obj = {
+//     nama: "Suyatna",
+//     kota: "Bekasi"
+// }
+
+// //array
+// var any = [1, 2, 3]
+
+// //function
+// var nama = function() {
+//         return "Suyatna";
+//     }
+//     //document.write(nama());
+
+// //looping
+// for (var i = 0; i <= 10; i++) {
+//     //bilangan ganjil
+//     if (i % 2 == 1) {
+//         document.write(i);
+//     }
+// }
+
+// //METODE SELECTOR
+// document.querySelectorAll("button")[1].onclick = function() {
+//     window.alert("hello");
+// }
+
+// function myAlert() {
+//     window.alert("hello world");
+// }
+
+// document.querySelectorAll("button")[0].addEventListener("click", function() {
+//     window.alert("hello");
+// })
+
+// $("button").click(function() {
+//     alert("hello .click() called.");
+// })
+
+// $("button:nth-child(2)").click(function() {
+//     alert("hello .click() called.");
+// })
